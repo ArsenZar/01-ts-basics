c704e49d8b3f7a48fd18d615bb5bd10056c96b46
ArsenZar <arsenzar@gmail.com>
2025-11-08 23:11:48 +0200

    Now U Can undestand Generic & Promice

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,13 @@
 
 // console.log(userResponse);
 
-//
+// ПОВНІСТЮ ВСЕ РОЗУМІННЯ ГЕНЕРИКІВ ТА ПРОМІСІВ
 
+function resolveAfterDelay<T>(value: T, delay: number): Promise<T> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(value), delay);
+  });
+}
 
+resolveAfterDelay<string>("Hello", 1000).then(console.log);
+resolveAfterDelay<number>(123, 500).then(console.log);
