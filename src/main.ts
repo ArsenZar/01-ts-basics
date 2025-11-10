53f47761da66b502ec489752c9621df631f1875f
ArsenZar <arsenzar@gmail.com>
2025-11-11 00:31:18 +0200

    learm main

--- src/main.ts
+++ src/main.ts
@@ -35,21 +35,50 @@
 import axios from "axios";
 const API = "https://jsonplaceholder.typicode.com";
 
-interface Post { id: number; userId: number; title: string; body: string; }
+// interface Post { id: number; userId: number; title: string; body: string; }
 
-async function fetchPosts(): Promise<Post[]> {
-  const res = await axios.get<Post[]>(
-    `${API}/posts`
-  );
-  return res.data;
+// async function fetchPosts(): Promise<Post[]> {
+//   const res = await axios.get<Post[]>(
+//     `${API}/posts`
+//   );
+//   return res.data;
+// }
+
+// // first type work with respone
+// const posts: Post[] = await fetchPosts();
+// console.log(posts[0].title);
+
+// // second type work with respone
+// fetchPosts().then(ps => {
+//   console.log("posts:", ps);  
+// });
+
+////////////////////////////////////////////////////
+
+// async function fetchList<T>(path: string): Promise<T[]> {
+//   const { data } = await axios.get<T[]>(`${API}/${path}`);
+//   return data;
+// }
+
+// interface Post { id: number; userId: number; title: string; body: string; }
+// interface User { id: number; name: string; email: string; }
+
+// fetchList<Post>("posts").then(list => console.log(list[0].title));
+// fetchList<User>("users").then(list => console.log(list[0].email));
+
+async function fetchList<T>(path: string): Promise<T[]> {
+  const { data } = await axios.get<T[]>(`${API}/${path}`);
+  return data;
 }
 
-// first type work with respone
-const posts: Post[] = await fetchPosts();
-console.log(posts[0].title);
+interface Post { id: number; userId: number; title: string; body: string; }
 
-// second type work with respone
-fetchPosts().then(ps => {
-  console.log("posts:", ps);  
-});
+fetchList<Post>("posts").then(list => console.log(list[0].title));
 
+function getFirst<T>(arr: T[]) {
+  return arr[0];
+}
+
+console.log(
+  getFirst<number>([1, 2, 3])
+ );
